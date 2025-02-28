a8e91c1b1c448af5793050d74bd4e606d6653a8b
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2025-02-28 16:03:31 +0900

    feat: project init

--- crawling/src/main/java/park/crawling/config/webClientConfig.java
+++ crawling/src/main/java/park/crawling/config/webClientConfig.java
@@ -1,2 +1,12 @@
-package park.crawling.config;public class webClientConfig {
+package park.crawling.config;
+
+import org.springframework.context.annotation.Bean;
+
+public class webClientConfig {
+//    @Bean
+//    public WebClient webClient() {
+//        return WebClient.builder()
+//                .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0")
+//                .build();
+//    }
 }
--- crawling/src/main/java/park/crawling/controller/crawlController.java
+++ crawling/src/main/java/park/crawling/controller/crawlController.java
@@ -1,2 +1,19 @@
-package park.crawling.controller;public class crawlController {
+package park.crawling.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class crawlController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "hi";
+    }
+
+    @PostMapping("/crawl")
+    public void crawl(){
+
+    }
 }
